d1fa6110709b32b508067b5c091dbb0a74cd2a73
juggalomc <oskarsdombrava@gmail.com>
2026-03-20 21:57:43 +0200

    Update useInvoiceHandlers.js

--- src/components/hooks/useInvoiceHandlers.js
+++ src/components/hooks/useInvoiceHandlers.js
@@ -1291,7 +1291,6 @@ export function useInvoiceHandlers(supabase, apartments, tariffs, invoices, wate
         loadScript('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'),
         loadScript('https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js')
       ]).then(async () => {
-        const html2canvas = window.html2canvas;
         const jsPDF = window.jspdf.jsPDF;
 
         try {
