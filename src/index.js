11b5995b4fec7642ac584a041ec10cec4f16dfdf
juggalomc <oskarsdombrava@gmail.com>
2026-03-20 22:55:41 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
